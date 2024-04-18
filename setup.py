61f3b092a0aa4bf66eb3962e5719d9659393116b
slavas <slavas@bk.ru>
2024-04-18 19:04:55 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_deps = ['sphericalmercator']
 
 if sys.version_info[0] == 3:               # Python version
 #    install_deps.append('python3-mapnik')
-    continue
+    pass
 else:
     install_deps.append('mapnik')
 
