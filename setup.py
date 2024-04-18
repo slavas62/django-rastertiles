82fd49457c156e2c0f4a8f1d1677c88417b54b67
slavas <slavas@bk.ru>
2024-04-18 14:37:14 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_packages
 
+install_deps = ['sphericalmercator']
+if get_dist('mapnik'):
+    install_deps.append('mapnik')
+else:
+    install_deps.append('python3-mapnik')
+
 setup(
     name='django-rastertiles',
-    version='0.0.2',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     long_description=open('README.rst').read(),
-    install_requires=[
-        'sphericalmercator',
-        'mapnik',
-    ],
+    
+    install_requires = install_deps,
 )
